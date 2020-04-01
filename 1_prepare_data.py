90881d11cf72a2809866634b83f7fac0c974f380
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2020-04-01 18:38:27 -0300

    Update 1_prepare_data.py

--- 1_prepare_data.py
+++ 1_prepare_data.py
@@ -1,44 +1,24 @@
 import numpy as np
 import cv2
 
-images_128 = []
-images_64 = []
-images_32 = []
-images_16 = []
-images_8 = []
-for i in range(1,101):
-    img = cv2.imread('train_images/128/' + str(i) + '.png')
+def prepare_images(n_images:int, output_shape:tuple,images_path:str, image_extension:str, output_path:str, output_name:str):
+    images = []
+    for i in range(1, n_images+1):
+        image = cv2.imread(images_path+str(i)+image_extension)
+        image = cv2.resize(image, output_shape)
+        image = (image - 127.5) + 127.5
+        images.append(image)
+    np.save(output_path+output_name, images)
+    return images
 
-    img64 = cv2.resize(img , (64, 128))
-    img32 = cv2.resize(img , (32, 64))
-    img16 = cv2.resize(img , (16, 32))
-    img8 = cv2.resize(img , (8, 16))
+# Paths
+images_path = 'train_images/128/'
+output_path = 'train_images/'
 
-    #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    #img64 = cv2.cvtColor(img64, cv2.COLOR_RGB2GRAY)
-    #img32 = cv2.cvtColor(img32, cv2.COLOR_RGB2GRAY)
-    #img16 = cv2.cvtColor(img16, cv2.COLOR_RGB2GRAY)
-    #img8 = cv2.cvtColor(img8, cv2.COLOR_RGB2GRAY)
+# Misc Variables
+n_images = 100
+image_extension = '.png'
+output_shape = (256,128)
+output_name = 'images_128.npy'
 
-    cv2.imwrite('train_images/64/' + str(i)+'.png', img64)
-    cv2.imwrite('train_images/32/' + str(i)+'.png', img32)
-    cv2.imwrite('train_images/16/' + str(i)+'.png', img16)
-    cv2.imwrite('train_images8/' + str(i)+'.png', img8)
-
-    img = (img - 127.5) / 127.5
-    img64 = (img64 - 127.5) / 127.5
-    img32 = (img32 - 127.5) / 127.5
-    img16 = (img16 - 127.5) / 127.5
-    img8 = (img8 - 127.5) / 127.5
-
-    images_128.append(img)
-    images_64.append(img64)
-    images_32.append(img32)
-    images_16.append(img16)
-    images_8.append(img8)
-
-np.save('train_images/images_128.npy', images_128)
-np.save('train_images/images_64.npy', images_64)
-np.save('train_images/images_32.npy', images_32)
-np.save('train_images/images_16.npy', images_16)
-np.save('train_images/images_8.npy', images_8)
+prepare_images(n_images, output_shape, images_path, image_extension, output_path, output_name)
